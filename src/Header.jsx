08f371b261122f0baa96d6928fb02db4cd74c22d
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-09-08 14:17:35 -0400

    add current user name

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,20 @@
 import { LogoutLink } from './LogoutLink';
 import './index.css'
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 export function Header() {
+const [user, setUser] = useState({})
+
+const getUserData = () => {
+axios.get("http://localhost:3000/users/current.json").then(response => 
+setUser(response.data)
+)
+}
+
+useEffect(getUserData, [])
+
   let authenticationLinks;
 
   if (localStorage.jwt === undefined) {
@@ -33,7 +45,7 @@ authenticationLinks = (
     <header>
    <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">{user.name}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
